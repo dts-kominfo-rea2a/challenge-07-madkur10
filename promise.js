574c32a6262bbc129a5239c9cb3de3019596de09
madkur10 <blackmadkur@gmail.com>
2022-06-22 11:03:53 +0700

    Commit Promise 1

--- promise.js
+++ promise.js
@@ -1,7 +1,18 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (perasaan) => {
+  try {
+    let teatherXXI = await promiseTheaterIXX();
+    let teatherCGV = await promiseTheaterVGC();
+  
+    let gabungTeather = teatherXXI.concat(teatherCGV);
+    
+    return gabungTeather.filter(hasilPerasaan => (hasilPerasaan.hasil === perasaan)).length;
+  } catch (error) {
+    console.log(error)
+  }
+};
 
 module.exports = {
   promiseOutput,
